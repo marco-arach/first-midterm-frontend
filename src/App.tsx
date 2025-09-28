c2d70a94c13c8984e51e15a1d91e6347574daf60
macuna <marcoantonioraiacuna@gmail.com>
2025-09-28 18:40:03 -0400

    Feature login completed.

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Register from "./pages/Register";
+import Login from "./pages/Login";
 import Main from "./pages/Main";
 
 const App: React.FC = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Navigate to="/register" />} />
+        <Route path="/" element={<Navigate to="/login" />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/main" element={<Main />} />
       </Routes>
     </Router>
